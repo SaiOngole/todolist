ce76ef92ed583018acc6df8ba5d7e8c8187c288f
SaiOngole <svo214@nyu.edu>
2014-05-26 00:23:01 -0400

    Initial Commit

---
+++ ToDoList/XYZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XYZAppDelegate.h
+//  ToDoList
+//
+//  Created by is3av on 5/26/14.
+//  Copyright (c) 2014 is3av. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
